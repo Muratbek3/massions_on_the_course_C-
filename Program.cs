bda65f3b29186edd1f1c69de7c19711dbc56ad17
Murat <bek.m4n@gmail.com>
2020-10-31 03:37:12 +0300

    massion_number_1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days_in_week= 7;
+            Console.WriteLine(days_in_week);
+            double weeks_in_year= 52.3;
+            System.Console.WriteLine(weeks_in_year);
+            double days_in_year = days_in_week * weeks_in_year;
+            System.Console.WriteLine(days_in_year);
+        }
+    }
+}
