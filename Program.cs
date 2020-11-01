b6ec9eeee74318225c525aeac9e2ca18f00e71fe
Murat <bek.m4n@gmail.com>
2020-11-01 15:32:00 +0300

    massion_9(if_else)

--- Program.cs
+++ Program.cs
@@ -1,19 +1,45 @@
 ﻿using System;
-
+using System.Threading;
 namespace variables
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            // int[] intArray = {1,2,3,4,0,5,6};
-            string i;
-            do
-            {
-                i = Console.ReadLine();
-                System.Console.WriteLine("hello world");
-            } while (i =="1");
-            
+            ConsoleKeyInfo keyInfo;
+
+            System.Console.WriteLine("Что бы начать антивирусную проверку нажмите Enter");
+            keyInfo = Console.ReadKey();
+           if(keyInfo.Key == ConsoleKey.Enter){
+               for (int i = 0; i < 10; i++)
+               {
+                   Console.Clear();
+                   System.Console.WriteLine("Проверка на вирусы");
+                    Thread.Sleep(100);
+                   Console.Clear();
+                   System.Console.WriteLine("Проверка на вирусы.");
+                   Thread.Sleep(100);
+                   Console.Clear();
+                   System.Console.WriteLine("Проверка на вирусы..");
+                   Thread.Sleep(100);
+                   Console.Clear();
+                   System.Console.WriteLine("Проверка на вирусы...");
+                   Thread.Sleep(100);
+                   Console.Clear();
+               }
+               System.Console.WriteLine("Проверка на вирусы...");
+                    
+                
+                
+               System.Console.WriteLine("Отлично! Зеленый уровень возможности заражения компьютера!");
+           }
+           else{
+               System.Console.WriteLine("Внимание! Красный уровень возможности заражения компьютера!");
+           }
+           
         } 
+        
     }
+    
 }
