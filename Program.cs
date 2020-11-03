4d4370bc916685e1f5643938d1669687d70676f8
Unknown <unknown@example.com>
2020-11-03 15:35:59 +0500

    massion_12(methods_2)

--- Program.cs
+++ Program.cs
@@ -7,18 +7,27 @@ namespace variables
         
         static void Main(string[] args)
         {
-
-            //запрашиваем строку у пользователя
-            System.Console.WriteLine("Введите пожалуйста слова или предложение");
-            string str = Console.ReadLine();
-			double StrNumber = StringNumber(str);
-			Console.WriteLine(StrNumber);
+            System.Console.WriteLine("Введите число для сравнения");
+            //Запрашиваем число для сравнения у пользователя
+            double CompNum = double.Parse(Console.ReadLine());
+            //Инициализируем метод и выводим возращаемое значение на экран
+            System.Console.WriteLine(CompZero(CompNum));
         } 
-        //Метод с параметром строки и возращающий ее длину в виде числа с плавающей точкой
-        static double StringNumber(string word)
+        //Метод который принимает значение с плавующей точкой и сравнивает его с нулем и возращает ответ в виде строки
+        static string CompZero(double nubmer)
         {
-          return word.Length;
+            
+            if (0>nubmer){
+            return "Число меньше нуля";
+            }
+            else if(0<nubmer){
+            return "Число больше нуля";
+            }
+            else if (0==nubmer){
+            return "Число равно нуля";
+            }
+         }
         }
     }
     
-}
+
